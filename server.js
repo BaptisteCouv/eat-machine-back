88bfe74dcfd781d445c86b2746c391f1e0f31fa0
BaptisteC <bcouavoux@positivethinking.tech>
2024-04-01 17:30:14 +0200

    Déploiement ajout de mongodb

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const mealsRoutes = require("./routes/listMealRoutes");
-// const foodStockNutritionalValueRoutes = require("./routes/foodStockNutritionalValueRoutes");
-// const categoryRoutes = require("./routes/categoryRoutes");
-// const foodBindRoutes = require("./routes/foodBindRoutes");
+const mealsRoutes = require("./routes/listMealRoutes");
+const foodStockNutritionalValueRoutes = require("./routes/foodStockNutritionalValueRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const foodBindRoutes = require("./routes/foodBindRoutes");
 
 app.use(express.json());
 app.use(cors());
@@ -24,25 +24,25 @@ app.use((req, res, next) => {
   next();
 });
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://couavouxgroupe:J93IQ8T81GUlnJBC@cluster-eat-machine.aofeed2.mongodb.net/eat_machine",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("Connexion à la base de données");
-//   })
-//   .catch((error) => {
-//     console.error("Erreur de connexion à la base de données:", error);
-//   });
+mongoose
+  .connect(
+    "mongodb+srv://couavouxgroupe:J93IQ8T81GUlnJBC@cluster-eat-machine.aofeed2.mongodb.net/eat_machine",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connexion à la base de données");
+  })
+  .catch((error) => {
+    console.error("Erreur de connexion à la base de données:", error);
+  });
 
-// app.use("/api/meals", mealsRoutes);
-// app.use("/api/foodsNutritional", foodStockNutritionalValueRoutes);
-// app.use("/api/categories", categoryRoutes);
-// app.use("/api/foodBind", foodBindRoutes);
+app.use("/api/meals", mealsRoutes);
+app.use("/api/foodsNutritional", foodStockNutritionalValueRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/foodBind", foodBindRoutes);
 
 app.use((req, res) => {
   res.json({ message: 'Connecté a l\'api' }); 
